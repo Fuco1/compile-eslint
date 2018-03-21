20b2d34894530818ca042e5b5a9bb9ab6d024cc7
Matus Goljer <matus.goljer@gmail.com>
2018-03-21 13:21:33 +0100

    Initial commit

---
+++ compile-eslint.el
@@ -0,0 +1,53 @@
+;;; compile-eslint.el --- compilation-error-regexp-alist rules for eslint -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Matúš Goljer
+
+;; Author: Matúš Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
+;; Version: 0.0.1
+;; Created: 21st March 2018
+;; Package-requires: ((dash "2.10.0"))
+;; Keywords: convenience, languages
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Rules to match eslint output in `compilation-mode'.
+
+;;; Code:
+
+(require 'compile)
+
+;;;###autoload
+(defun compile-eslint--find-filename ()
+  "Find the filename for current error."
+  (save-match-data
+    (save-excursion
+      (when (re-search-backward (rx bol (group "/" (+ any)) eol))
+        (list (match-string 1))))))
+
+;;;###autoload
+(let ((form `(eslint
+              ,(rx-to-string
+                '(and (group (group (+ digit)) ":" (group (+ digit)))
+                      (+ " ") (or "error" "warning")))
+              compile-eslint--find-filename
+              2 3 2 1)))
+  (if (assq 'eslint compilation-error-regexp-alist-alist)
+      (setf (cdr (assq 'eslint compilation-error-regexp-alist-alist)) (cdr form))
+    (push form compilation-error-regexp-alist-alist)))
+
+(provide 'compile-eslint)
+;;; compile-eslint.el ends here
